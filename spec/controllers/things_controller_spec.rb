cfd801022a26a1be2084f10b45799ebfd8b29bb2
Viktor Lazarev <taurus101v@gmail.com>
2015-10-25 00:50:35 +0400

    Fix controller specs

--- spec/controllers/things_controller_spec.rb
+++ spec/controllers/things_controller_spec.rb
@@ -8,13 +8,9 @@ describe ThingsController do
       expect(response).to redirect_to thing_path Thing.last
     end
 
-    it 'shows error message if thing invalid', type: :request do
-      post '/things', thing: { name: '' }
-      p response.body
-      #:follow_redirect!
-#      p page.methods, page.body, page.html, page.status_code
-      p response.body
-      expect(response.body).to have_selector '.error'
+    it 'renders "new" form again if thing invalid', type: :controller do
+      post :create, thing: { name: '' }
+      expect(response).to render_template :new
     end
   end
 end
