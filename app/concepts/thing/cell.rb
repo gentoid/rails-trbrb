c7482324052603732dcfbb513354a1d235c45cc6
Viktor Lazarev <taurus101v@gmail.com>
2015-10-26 00:58:12 +0400

    Move logick to Cell class

--- app/concepts/thing/cell.rb
+++ app/concepts/thing/cell.rb
@@ -1,5 +1,17 @@
 class Thing::Cell < Cell::Concept
+
   def show
     render
   end
+
+  private
+
+  def name_link
+    link_to model.name, thing_path(model)
+  end
+
+  def created_at
+    model.created_at
+  end
+
 end
