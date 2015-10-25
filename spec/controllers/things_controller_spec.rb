25d768987bd68b40863841d668430548f6d41ded
Viktor Lazarev <taurus101v@gmail.com>
2015-10-25 10:21:48 +0400

    Add specs for thing controller #new

--- spec/controllers/things_controller_spec.rb
+++ spec/controllers/things_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'capybara/rspec'
 
 describe ThingsController do
-  describe '#create' do
-    render_views
+  render_views
 
+  describe '#create' do
     it 'creates and redirects if thing valid' do
       post :create, thing: { name: 'All is ok' }
-      
       expect(response).to redirect_to thing_path Thing.last
     end
 
@@ -17,14 +16,20 @@ describe ThingsController do
   end
 
   describe '#edit' do
-    render_views
-
     let (:thing) { Thing::Create.(thing: { name: 'Rails', description: 'Web dev' }).model }
 
     it 'shows name field as disabled' do
       get :edit, id: thing.id
       expect(response).to render_template :new
       expect(response.body).to have_selector 'form #thing_name.readonly[value="Rails"]'
+    end
+  end
+
+  describe '#new' do
+    it 'shows form with enabled name field' do
+      get :new
+      expect(response.body).to have_selector 'form #thing_name'
+      expect(response.body).not_to have_selector 'form #thing_name.readonly'
     end
   end
 end
