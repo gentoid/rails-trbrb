68e1eb62d615bb26bfe2c0a0c3df58d4f0d4c36e
Viktor Lazarev <taurus101v@gmail.com>
2015-10-29 01:01:52 +0400

    Fix cell specs

--- spec/concepts/thing/cell_spec.rb
+++ spec/concepts/thing/cell_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 require 'cells'
 
-describe Thing::Cell do
-  let (:rails) { Thing::Create.( thing: { name: 'Rails' } ) }
-  let (:trb) { Thing::Create.( thing: { name: 'Trailblazer' } ) }
+describe Thing::Cell, type: :cell do
+  let (:rails) { Thing::Create.( thing: { name: 'Rails' } ).model }
+  let (:trb) { Thing::Create.( thing: { name: 'Trailblazer' } ).model }
 
   subject { concept 'thing/cell', collection: [trb, rails], last: rails }
+
+  controller HomeController
 
   it 'renders things' do
     expect(subject).to have_selector '.columns'
