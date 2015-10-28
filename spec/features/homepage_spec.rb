d2ec2301ee5d298d0d79b9e0cfba3008a10aef5e
Viktor Lazarev <taurus101v@gmail.com>
2015-10-29 01:42:56 +0400

    Simplify homepage spec, since cell is tested in its own spec

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,14 +2,8 @@ require 'rails_helper'
 
 feature 'Homepage' do
   scenario 'contains things' do
-    Thing::Create.( thing: { name: 'Trailblazer' } )
     Thing::Create.( thing: { name: 'Rails' } )
-
     visit '/'
-
-    expect(page.body).to have_selector '.columns .header a', 'Rails'
-    expect(page.body).to have_selector '.columns.end', 1
-    expect(page.body).to have_selector '.columns.end .header a', 'Trailblazer'
-
+    expect(page.body).to have_selector '.header a', 'Rails'
   end
 end
