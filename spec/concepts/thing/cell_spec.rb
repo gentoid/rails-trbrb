374c9c6692b1191818a9778da6a53efd1ca78b50
Viktor Lazarev <taurus101v@gmail.com>
2015-10-29 01:39:57 +0400

    Update specs accordingly

--- spec/concepts/thing/cell_spec.rb
+++ spec/concepts/thing/cell_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 require 'cells'
 require 'capybara/rspec'
 
-describe Thing::Cell, type: :cell do
-  let (:rails) { Thing::Create.( thing: { name: 'Rails' } ).model }
-  let (:trb) { Thing::Create.( thing: { name: 'Trailblazer' } ).model }
-
-  subject { concept 'thing/cell', collection: [rails, trb], last: trb }
-
+describe Thing::Cell::Grid, type: :cell do
   controller HomeController
 
-  it 'renders things', type: :feature do
-    expect(subject).to have_selector '.columns .header a', text: 'Rails'
-    expect(subject).not_to have_selector '.columns.end .header a', text: 'Rails'
-    expect(subject).to have_selector '.columns.end .header a', text: 'Trailblazer'
+  it 'renders grid of things', type: :feature do
+    Thing::Create.( thing: { name: 'Trailblazer' } ).model
+    Thing::Create.( thing: { name: 'Rails' } ).model
+
+    html = concept('thing/cell/grid').show
+
+    expect(html).to have_selector '.columns .header a', text: 'Rails'
+    expect(html).not_to have_selector '.columns.end .header a', text: 'Rails'
+    expect(html).to have_selector '.columns.end .header a', text: 'Trailblazer'
   end
 end
