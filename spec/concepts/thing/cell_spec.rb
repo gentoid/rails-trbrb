7f2e957b19b2074302c9e05810c6b88ee3e164b0
Viktor Lazarev <taurus101v@gmail.com>
2015-10-29 01:15:27 +0400

    Add more expectations in cell spec

--- spec/concepts/thing/cell_spec.rb
+++ spec/concepts/thing/cell_spec.rb
@@ -6,11 +6,13 @@ describe Thing::Cell, type: :cell do
   let (:rails) { Thing::Create.( thing: { name: 'Rails' } ).model }
   let (:trb) { Thing::Create.( thing: { name: 'Trailblazer' } ).model }
 
-  subject { concept 'thing/cell', collection: [trb, rails], last: rails }
+  subject { concept 'thing/cell', collection: [rails, trb], last: trb }
 
   controller HomeController
 
   it 'renders things', type: :feature do
-    expect(subject).to have_selector '.columns'
+    expect(subject).to have_selector '.columns .header a', text: 'Rails'
+    expect(subject).not_to have_selector '.columns.end .header a', text: 'Rails'
+    expect(subject).to have_selector '.columns.end .header a', text: 'Trailblazer'
   end
 end
