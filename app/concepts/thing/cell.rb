687b4772eecc98c2ac540bddd9b7244e9475c1e7
Viktor Lazarev <taurus101v@gmail.com>
2015-10-29 01:19:52 +0400

    Refactoring: add Grid cell to encapsulate grid on homepage

--- app/concepts/thing/cell.rb
+++ app/concepts/thing/cell.rb
@@ -25,4 +25,11 @@ class Thing::Cell < Cell::Concept
     classes
   end
 
+  class Grid < Cell::Concept
+    def show
+      things = Thing.latest
+      concept 'thing/cell', collection: things, last: things.last
+    end
+  end
+
 end
