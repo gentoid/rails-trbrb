8a8c18d668200cf38e966fd11ca9e602e2e86714
Viktor Lazarev <taurus101v@gmail.com>
2015-10-30 01:56:05 +0400

    Add comments resource: route. controller, model, concept

---
+++ app/concepts/comment/crud.rb
@@ -0,0 +1,19 @@
+class Comment < ActiveRecord::Base
+  class Create < Trailblazer::Operation
+    include Model
+    model Comment, :create
+
+    def process(params)
+      validate params[:comment] do |f|
+        f.save
+      end
+    end
+
+    private
+
+    def setup_model!(params)
+      model.thing = Thing.find_by_id params[:thing_id]
+    end
+
+  end
+end
