5103cad9f7af7023942425f7c7e7fd6d96b9662c
Viktor Lazarev <taurus101v@gmail.com>
2015-10-31 02:07:35 +0400

    Add more complex logic on comment creating action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,14 @@ class CommentsController < ApplicationController
   end
 
   def create
-    run Comment::Create
+    run Comment::Create do |op|
+      flash[:notice] = "Created comment for '#{op.model.thing.name}'"
+
+      return redirect_to thing_path op.model.thing
+    end
+
+    @thing = Thing.find params[:thing_id]
+    render :new
   end
 
 end
