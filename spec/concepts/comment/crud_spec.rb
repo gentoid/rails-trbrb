fca0222afcc1d8f3c0e536dd998960f77cd993ef
Viktor Lazarev <taurus101v@gmail.com>
2015-11-01 04:13:57 +0400

    Add spec for Comment::Create

---
+++ spec/concepts/comment/crud_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Comment::Create do
+  let (:thing) { Thing::Create.( thing: { name: 'Ruby' } ).model }
+
+  it 'persists valid' do
+    res, op = Comment::Create.run(
+      comment: {
+        body: 'Fantastic!',
+        weight: '1',
+        user: { email: 'john@example.com' }
+      },
+      thing_id: thing.id,
+    )
+    comment = op.model
+
+    expect(comment.persisted?).to eq true
+    expect(comment.body).to eq 'Fantastic!'
+    expect(comment.weight).to eq 1
+  end
+end
